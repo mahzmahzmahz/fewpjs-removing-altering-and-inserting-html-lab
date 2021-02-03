047160ce9347c2afbf33f360c2c600075adc8ddf
Alex Marz <alexhmarz@gmail.com>
2021-02-02 19:42:48 -0500

    Starting to click

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+document.getElementById('main').remove()
+
+let newHeader = document.createElement('h1')
+document.head.appendChild(newHeader)
+
+newHeader.className = "victory"
+newHeader.id = "victory"
+
+let p = document.createElement('p')
+    p.innerHTML = "ALEX is the champion"
+newHeader.appendChild(p)
